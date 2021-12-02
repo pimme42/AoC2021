6a0ccd3dc01935a87a5af08cc8ebcd1e8232fd9a
Tobias <github@pimme.org>
2021-12-02 07:36:54 +0100

    Stage 1 and 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-AoC2021
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ test/y2021_test.dart
@@ -0,0 +1,8 @@
+import 'package:y2021/y2021.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
